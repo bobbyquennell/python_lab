280c24de59680b17713ae16c68cf7befb8c74cde
Bobby Quennell <bobbyquennell@gmail.com>
2015-06-29 15:44:48 +1000

    Leap Years Kata-----RED: add a new test case: test_atypical_common_year, which fails

--- coding_dojo_tdd/test_leapyear.py
+++ coding_dojo_tdd/test_leapyear.py
@@ -5,3 +5,6 @@ def test_normal_leap_year():
     assert is_leap(1996)
 def test_normal_common_year():
      assert not is_leap(2001)
+     
+def test_atypical_common_year():
+     assert not is_leap(1900)
