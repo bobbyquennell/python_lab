7b26bacd2716b5bfd1bfb117883bd60d05409ba1
Bobby Quennell <bobbyquennell@gmail.com>
2015-06-29 16:25:28 +1000

    Leap Years Kata------Refactor step: extract the method

--- coding_dojo_tdd/test_leapyear.py
+++ coding_dojo_tdd/test_leapyear.py
@@ -1,8 +1,9 @@
 def is_leap(year):
-    if year % 100 == 0 and not year %400 == 0:
+    if is_atypical_common_year(year):
         return False
     return year % 4 == 0
-
+def is_atypical_common_year(year):
+      return year % 100 == 0 and not year % 400 == 0
 def test_normal_leap_year():
     assert is_leap(1996)
 def test_normal_common_year():
