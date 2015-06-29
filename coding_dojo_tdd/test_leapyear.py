3ec5027eb29ad0fca5b8a5dfe3957738985c4802
Bobby Quennell <bobbyquennell@gmail.com>
2015-06-29 17:09:25 +1000

    Leap Years Kata------add some comments for the First Kata written in python

--- coding_dojo_tdd/test_leapyear.py
+++ coding_dojo_tdd/test_leapyear.py
@@ -1,3 +1,15 @@
+# Code Kata----Leap Years
+# Write a function that returns true or false depending on whether its
+# input integer is a leap year or not. A leap year is divisible by 4, but is not 
+# otherwise divisible by 100, unless it is also divisible by 400.
+# Examples:
+# 1996 ---> true
+# 2001 ---> false
+# 2000 ---> true
+# 1900 ---> false
+
+
+
 def is_leap(year):
     return is_typical_leap_year(year) and not is_atypical_common_year(year)
     
